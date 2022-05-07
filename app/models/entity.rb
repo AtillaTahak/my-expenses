925c03dd62611e997b18c38db51e85249a8206fc
atillatahak <atillatahaa@gmail.com>
2022-05-07 11:59:36 +0300

    fixed linter

--- app/models/entity.rb
+++ app/models/entity.rb
@@ -1,6 +1,6 @@
 class Entity < ApplicationRecord
   belongs_to :user
   belongs_to :group
-  validates :name, presence: true , acceptance: { message: 'Should not be empty' }
-  validates :amount, presence: true , acceptance: { message: 'Should not be empty' }
+  validates :name, presence: true, acceptance: { message: 'Should not be empty' }
+  validates :amount, presence: true, acceptance: { message: 'Should not be empty' }
 end
